0e509d5b35f6b25b1ec98f34f03b535b8b1c8820
EiffL <fr.eiffel@gmail.com>
2020-05-01 22:42:50 +0200

    Adds a setup.py to look more professional

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup, find_packages
+
+setup(name='jax_cosmo',
+      version='0.0.1',
+      description='Differentiable Python Cosmology Library',
+      author='Francois Lanusse',
+      packages=find_packages(),
+      install_requires=['jax', 'jaxlib']
+     )
