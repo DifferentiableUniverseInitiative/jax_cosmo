Fixes a bunch of normalization problems

jax_cosmo/angular_cl.py
@@ -4,6 +4,7 @@ from functools import partial
 import jax.numpy as np
 from jax import vmap, lax, jit
 
+import jax_cosmo.constants as const
 from jax_cosmo.utils import z2a, a2z
 from jax_cosmo.scipy.integrate import simps
 import jax_cosmo.background as bkgrd
@@ -73,18 +74,19 @@ def angular_cl(cosmo, ell, probes, amin=0.002):
 
     # Define an ordering for the blocks of the signal vector
     cl_index = np.array(_get_cl_ordering(probes))
-
     # Compute all combinations of tracers
     @jit
     def combine_kernels(inds):
       return kernels[inds[0]] * kernels[inds[1]]
-
     # Now kernels has shape [ncls, na]
     kernels = lax.map(combine_kernels, cl_index)
-    # We transpose the result just to make sure that na is first
-    return (pk * kernels * bkgrd.dchioverda(cosmo, a)/a**2).T
 
-  return simps(integrand, amin, 1., 512)
+    result = pk * kernels * bkgrd.dchioverda(cosmo, a) / np.clip(chi**2, 1.)
+
+    # We transpose the result just to make sure that na is first
+    return result.T
+
+  return simps(integrand, amin, 1., 512) / const.c**2
 
 def noise_cl(ell, probes):
   """

jax_cosmo/probes.py
@@ -43,7 +43,7 @@ class WeakLensing(container):
     return len(pzs)
 
   def constant_factor(self, cosmo):
-    return 3.0 * const.H0**2 * cosmo.Omega_m / (2.0 * const.c**2)
+    return 3.0 * const.H0**2 * cosmo.Omega_m / 2.0 / const.c
 
   @jit
   def radial_kernel(self, cosmo, z):
@@ -68,14 +68,100 @@ class WeakLensing(container):
       chi_prime = bkgrd.radial_comoving_distance(cosmo, z2a(z_prime))
       # Stack the dndz of all redshift bins
       dndz = np.stack([pz(z_prime) for pz in pzs], axis=0)
-      return dndz * np.clip(chi_prime - chi, 0) / (chi_prime + 1e-5)
-    return np.squeeze(simps(integrand, z, zmax, 256))
+      return dndz * np.clip(chi_prime - chi, 0) / np.clip(chi_prime, 1.)
+
+    result = simps(integrand, z, zmax, 256) * (1. + z ) * chi
+
+    return np.squeeze(result)
 
   def ell_factor(self, ell):
     """
     Computes the ell dependent factor for this probe.
     """
     return np.sqrt((ell-1)*(ell)*(ell+1)*(ell+2))/(ell+0.5)**2
+
+  def noise(self):
+    """
+    Returns the noise power for all redshifts
+    return: shape [nbins]
+    """
+    # Extract parameters
+    pzs = self.params[0]
+    print("Careful! lensing noise is still a placeholder")
+    # TODO: find where to store this
+    sigma_e = 0.26
+    n_eff = 20.
+
+    # retrieve number of galaxies in each bins
+    # ngals = np.array([pz.ngals for pz in pzs])
+    # TODO: find how to properly compute the noise contributions
+    ngals = np.array([1. for pz in pzs])
+
+    # compute n_eff per bin
+    n_eff_per_bin = n_eff * ngals/np.sum(ngals)
+
+    # work out the number density per steradian
+    steradian_to_arcmin2 = 11818102.86004228
+
+    return sigma_e**2/(n_eff_per_bin * steradian_to_arcmin2)
+
+
+@register_pytree_node_class
+class NumberCounts(container):
+  """
+  Class representing a galaxy clustering probe, with a bunch of bins
+
+  Parameters:
+  -----------
+  redshift_bins: nzredshift distributions
+
+  Configuration:
+  --------------
+  has_rsd....
+  """
+  def __init__(self, redshift_bins, bias,
+               has_rsd=False,
+               **kwargs):
+    super(NumberCounts, self).__init__(redshift_bins,
+                                       bias,
+                                       has_rsd=has_rsd,
+                                       **kwargs)
+  @property
+  def n_tracers(self):
+    """
+    Returns the number of tracers for this probe, i.e. redshift bins
+    """
+    # Extract parameters
+    pzs = self.params[0]
+    return len(pzs)
+
+  def constant_factor(self, cosmo):
+    #TODO: use the correct prefactor
+    return 1.0
+
+  @jit
+  def radial_kernel(self, cosmo, z):
+    """
+    Compute the radial kernel for all nz bins in this probe.
+
+    Returns:
+    --------
+    radial_kernel: shape (nbins, nz)
+    """
+    z = np.atleast_1d(z)
+    # Extract parameters
+    pzs, bias = self.params
+    # stack the dndz of all redshift bins
+    dndz = np.stack([pz(z) for pz in pzs], axis=0)
+
+    return dndz * bias(z) * bkgrd.H(cosmo, z2a(z))
+
+  def ell_factor(self, ell):
+    """
+    Computes the ell dependent factor for this probe.
+    """
+    # TODO: return the correct factor
+    return 1.
 
   def noise(self):
     """