972b556d2c72e0697abc5668baa4567ff8692606
Francois <francois@orioncloud.fr>
2020-05-23 21:01:25 +0200

    Adds support for reading the version number

--- jax_cosmo/__init__.py
+++ jax_cosmo/__init__.py
@@ -3,6 +3,15 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from importlib.metadata import PackageNotFoundError
+from importlib.metadata import version
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
 import jax_cosmo.angular_cl as cl
 import jax_cosmo.background as background
 import jax_cosmo.bias as bias
