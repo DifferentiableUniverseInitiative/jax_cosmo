f0be31a025526cfdb3f8e4a046c8bb4c591f140a
EiffL <fr.eiffel@gmail.com>
2020-06-07 23:11:08 +0200

    Updates background to use new spline

--- tests/test_background.py
+++ tests/test_background.py
@@ -36,15 +36,15 @@ def test_distances_flat():
 
     chi_ccl = ccl.comoving_radial_distance(cosmo_ccl, a)
     chi_jax = bkgrd.radial_comoving_distance(cosmo_jax, a) / cosmo_jax.h
-    assert_allclose(chi_ccl, chi_jax, rtol=0.5e-2)
+    assert_allclose(chi_ccl, chi_jax, rtol=1e-3)
 
     chi_ccl = ccl.comoving_angular_distance(cosmo_ccl, a)
     chi_jax = bkgrd.transverse_comoving_distance(cosmo_jax, a) / cosmo_jax.h
-    assert_allclose(chi_ccl, chi_jax, rtol=0.5e-2)
+    assert_allclose(chi_ccl, chi_jax, rtol=1e-3)
 
     chi_ccl = ccl.angular_diameter_distance(cosmo_ccl, a)
     chi_jax = bkgrd.angular_diameter_distance(cosmo_jax, a) / cosmo_jax.h
-    assert_allclose(chi_ccl, chi_jax, rtol=0.5e-2)
+    assert_allclose(chi_ccl, chi_jax, rtol=1e-3)
 
 
 def test_growth():
@@ -72,12 +72,12 @@ def test_growth():
     )
 
     # Test array of scale factors
-    a = np.linspace(0.01, 1.0)
+    a = np.linspace(0.1, 1.0)
 
     gccl = ccl.growth_factor(cosmo_ccl, a)
     gjax = bkgrd.growth_factor(cosmo_jax, a)
 
-    assert_allclose(gccl, gjax, rtol=1e-2)
+    assert_allclose(gccl, gjax, rtol=1e-3)
 
 
 def test_growth_rate():
@@ -105,12 +105,12 @@ def test_growth_rate():
     )
 
     # Test array of scale factors
-    a = np.linspace(0.01, 1.0)
+    a = np.linspace(0.1, 1.0)
 
     fccl = ccl.growth_rate(cosmo_ccl, a)
     fjax = bkgrd.growth_rate(cosmo_jax, a)
 
-    assert_allclose(fccl, fjax, rtol=1e-2)
+    assert_allclose(fccl, fjax, rtol=1e-3)
 
 
 def test_growth_rate_gamma():
@@ -140,12 +140,12 @@ def test_growth_rate_gamma():
     )
 
     # Test array of scale factors
-    a = np.linspace(0.01, 1.0)
+    a = np.linspace(0.1, 1.0)
 
     fccl = ccl.growth_rate(cosmo_ccl, a)
     fjax = bkgrd.growth_rate(cosmo_jax, a)
 
-    assert_allclose(fccl, fjax, rtol=1e-2)
+    assert_allclose(fccl, fjax, rtol=5e-3)
 
 
 def test_growth_gamma():
@@ -174,9 +174,9 @@ def test_growth_gamma():
     )
 
     # Test array of scale factors
-    a = np.linspace(0.01, 1.0)
+    a = np.linspace(0.1, 1.0)
 
     gccl = ccl.growth_factor(cosmo_ccl, a)
     gjax = bkgrd.growth_factor(cosmo_jax, a)
 
-    assert_allclose(gccl, gjax, rtol=1e-2)
+    assert_allclose(gccl, gjax, rtol=1e-3)
