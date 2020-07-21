b4a0b5267f9c03b963265763420aec3836907f62
David Kirkby <dkirkby@uci.edu>
2020-07-21 16:36:23 -0700

    Implement sparse inv

--- jax_cosmo/sparse.py
+++ jax_cosmo/sparse.py
@@ -14,6 +14,17 @@ from jax import jit
 from jax import vmap
 
 
+def check_sparse(sparse, square=False):
+    """Check for a valid sparse matrix.
+    """
+    sparse = np.array(sparse)
+    if sparse.ndim != 3:
+        raise ValueError("Expected 3D array of sparse diagonals.")
+    if square and (sparse.shape[0] != sparse.shape[1]):
+        raise ValueError("Can only invert a square matrix.")
+    return sparse
+
+
 @jit
 def to_dense(sparse):
     """Convert a sparse matrix to its dense equivalent.
@@ -29,4 +40,28 @@ def to_dense(sparse):
         2D array of shape (ny * ndiag, nx * ndiag) with the same dtype
         as the input array.
     """
+    sparse = check_sparse(sparse)
     return np.vstack(vmap(lambda row: np.hstack(vmap(np.diag)(row)))(sparse))
+
+
+@jit
+def inv(sparse):
+    """Calculate the inverse of a square matrix in sparse format.
+
+    We currently assume that the matrix is invertible and you should not
+    trust the answer unless you know this is true (because jax.numpy.linalg.inv
+    has this behavior).
+
+    Parameters
+    ----------
+    sparse : array
+        3D array of shape (n, n, ndiag) of block diagonal elements.
+
+    Returns
+    -------
+    array
+        3D array of shape (n, n, ndiag) of block diagonal elements
+        representing the inverse matrix.
+    """
+    sparse = check_sparse(sparse, square=True)
+    return np.transpose(np.linalg.inv(np.transpose(sparse, (2, 0, 1))), (1, 2, 0))
--- tests/test_sparse.py
+++ tests/test_sparse.py
@@ -1,6 +1,6 @@
 import jax.numpy as jnp
 import numpy as numpy
-from numpy.testing import assert_allclose, assert_array_equal
+from numpy.testing import assert_allclose, assert_array_equal, assert_raises
 
 from jax_cosmo.sparse import *
 
@@ -21,3 +21,19 @@ def test_to_dense():
         ]
     )
     assert_array_equal(X_dense, X_answer)
+
+    with assert_raises(ValueError):
+        to_dense([1, 2, 3])
+
+    with assert_raises(ValueError):
+        to_dense(jnp.ones((2, 3, 4, 5)))
+
+
+def test_inv():
+    X_sparse = jnp.array([[[1.0, 1.0], [1.0, 1.0]], [[1.0, 1.0], [2.0, 2.0]]])
+    X_inv_sparse = inv(X_sparse)
+    X_answer = jnp.array([[[2.0, 2.0], [-1.0, -1.0]], [[-1.0, -1.0], [1.0, 1.0]]])
+    assert_allclose(X_inv_sparse, X_answer)
+
+    with assert_raises(ValueError):
+        inv(jnp.ones((2, 3, 4)))
