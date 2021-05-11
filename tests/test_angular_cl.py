d991f56fd17027fb3e95e0d24485ae992cbfb483
LANZIERI Denise <deniselanzieri@gmail.com>
2021-05-11 15:39:55 +0200

    test for a single plane redshift.

--- tests/test_angular_cl.py
+++ tests/test_angular_cl.py
@@ -11,7 +11,7 @@ from jax_cosmo.angular_cl import angular_cl
 from jax_cosmo.angular_cl import gaussian_cl_covariance
 from jax_cosmo.bias import constant_linear_bias
 from jax_cosmo.bias import inverse_growth_linear_bias
-from jax_cosmo.redshift import smail_nz
+from jax_cosmo.redshift import smail_nz, single_plane, kde_nz
 from jax_cosmo.sparse import to_dense
 
 
@@ -52,6 +52,49 @@ def test_lensing_cl():
     cl_ccl = ccl.angular_cl(cosmo_ccl, tracer_ccl, tracer_ccl, ell)
     cl_jax = angular_cl(cosmo_jax, ell, [tracer_jax])
 
+    assert_allclose(cl_ccl, cl_jax[0], rtol=1.0e-2)
+    
+    
+def test_lensing_cl_single_plane():
+    # We first define equivalent CCL and jax_cosmo cosmologies
+    cosmo_ccl = ccl.Cosmology(
+        Omega_c=0.3,
+        Omega_b=0.05,
+        h=0.7,
+        sigma8=0.8,
+        n_s=0.96,
+        Neff=0,
+        transfer_function="eisenstein_hu",
+        matter_power_spectrum="halofit",
+    )
+
+    cosmo_jax = Cosmology(
+        Omega_c=0.3,
+        Omega_b=0.05,
+        h=0.7,
+        sigma8=0.8,
+        n_s=0.96,
+        Omega_k=0.0,
+        w0=-1.0,
+        wa=0.0,
+    )
+
+    # Define a redshift distribution
+    z0=1.
+    z = np.linspace(0, 5.0, 1024)
+    pz = np.zeros_like(z)
+    pz[np.argmin(abs(z0 - z))] = 1. 
+    nzs_s=kde_nz(z, pz, bw=0.01)
+    nz=single_plane(z0)
+    tracer_ccl = ccl.WeakLensingTracer(cosmo_ccl, (z, nzs_s(z)), use_A_ia=False)
+    tracer_jax = probes.WeakLensing([nz])
+
+    # Get an ell range for the cls
+    ell = np.logspace(0.1, 4)
+
+    # Compute the cls
+    cl_ccl = ccl.angular_cl(cosmo_ccl, tracer_ccl, tracer_ccl, ell)
+    cl_jax = angular_cl(cosmo_jax, ell, [tracer_jax])
     assert_allclose(cl_ccl, cl_jax[0], rtol=1.0e-2)
 
 
