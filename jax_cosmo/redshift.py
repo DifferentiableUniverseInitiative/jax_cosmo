f2ca55b0284556306a5c101507f5104ab4c940b9
Natalie B. Hogg <mail@nataliebhogg.com>
2025-06-27 14:50:50 +0200

    Adds the redshift distribution from Martinet et al. (2021) (#131)
    * adds the redshift distribution from Martinet et al. (2021)
    
    ---------
    
    Co-authored-by: Francois Lanusse <fr.eiffel@gmail.com>

--- jax_cosmo/redshift.py
+++ jax_cosmo/redshift.py
@@ -9,7 +9,7 @@ from jax_cosmo.scipy.integrate import simps
 
 steradian_to_arcmin2 = 11818102.86004228
 
-__all__ = ["smail_nz", "kde_nz", "delta_nz"]
+__all__ = ["smail_nz", "fu_nz", "kde_nz", "delta_nz"]
 
 
 class redshift_distribution(container):
@@ -75,6 +75,34 @@ class smail_nz(redshift_distribution):
     def pz_fn(self, z):
         a, b, z0 = self.params
         return z**a * np.exp(-((z / z0) ** b))
+
+
+@register_pytree_node_class
+class fu_nz(redshift_distribution):
+    """Defines the redshift distribution from Fu et al. (2008), as used in Martinet et al. (2021)
+
+    Formula: n(z) = A * (z^a + z^(ab)) / (z^b + c)
+
+    Parameters:
+    -----------
+    a: float
+        Shape parameter (0.4710 in Martinet et al. 2021)
+    b: float
+        Shape parameter (5.1843 in Martinet et al. 2021)
+    c: float
+        Shape parameter (0.7259 in Martinet et al. 2021)
+    gals_per_arcmin2: float
+        Number of galaxies per sq arcmin (30 in Martinet et al. 2021)
+
+    References:
+    -----------
+    Fu et al. (2008) - https://arxiv.org/abs/0712.0884
+    Martinet et al. (2021) - https://arxiv.org/abs/2010.07376
+    """
+
+    def pz_fn(self, z):
+        a, b, c = self.params
+        return (z**a + z ** (a * b)) / (z**b + c)
 
 
 @register_pytree_node_class
